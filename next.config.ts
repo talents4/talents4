aebf71ba6a36b7439ed05492adbe19e9d0796d85
carollinyrezende52-design <carollinyrezende52@gmail.com>
2026-03-16 21:44:18 +0100

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@supabase/ssr"],
-  },
-};
-
-export default nextConfig;
